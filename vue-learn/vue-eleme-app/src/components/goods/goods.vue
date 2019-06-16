3236e0bb7be7ae61c530eaed73a0a6faf66715e7
qwerasdfqwerasdf <1486125411@qq.com>
2019-06-16 15:16:41 +0800

    vuex

--- vue-learn/vue-eleme-app/src/components/goods/goods.vue
+++ vue-learn/vue-eleme-app/src/components/goods/goods.vue
@@ -3,84 +3,85 @@
     <div class="goods">
       <div class="menu-wrapper" ref="menuWrapper">
         <ul>
-          <li 
-          v-for="(item, index) in goods"
-          :key="index"
-          class="menu-item"
-          :class="{'current' : currentIndex === index}"
-          @click="selectMenu(index, $event)"
+          <li
+            v-for="(item,index) in goods"
+            :key="index"
+            class="menu-item"
+            :class="{'current':currentIndex===index}"
+            @click="selectMenu(index,$event)"
           >
-          <span class="text border-1px">
-            <span class="icon" v-show="item.type > 0" :class="classMap[item.type]"></span>
-            {{item.name}}
-          </span>
+            <span class="text border-1px">
+              <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
+              {{item.name}}
+            </span>
           </li>
         </ul>
       </div>
       <div class="foods-wrapper" ref="foodsWrapper">
         <ul>
-          <li v-for="(item, index) in goods" 
-          :key="index"
-          class="food-list"
-          ref="foodList"
-          >
-          <h1 class="title">{{item.name}}</h1>
-          <ul>
-            <li v-for="(food, index) in item.foods"
-            :key="index" class="food-item border-1px">
-            <div class="icon">
-              <img :src="food.icon" width="57" height="57" alt="">
-            </div>
-            <div class="content">
-              <h2 class="name">{{food.name}}</h2>
-              <p class="desc">{{food.description}}</p>
-              <div class="extra">
-                <span class="count">月售{{food.sellCount}}份</span>
-                <span>好评率{{food.rating}}%</span>
-              </div>
-              <div class="price">
-                <span class="now">¥{{food.price}}</span>
-                <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
-              </div>
-              <!-- 添加 -->
-              <div class="cartcontrol-wrapper">
-                <v-cartcontrol :food="food" @add ="addFood"></v-cartcontrol>
-              </div>
-            </div>
-            </li>
-          </ul>
+          <li v-for="(item,index) in goods" :key="index" class="food-list" ref="foodList">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li
+                @click="selectFood(food, $event)"
+                v-for="(food,index) in item.foods"
+                :key="index"
+                class="food-item border-1px"
+              >
+                <div class="icon">
+                  <img width="57" height="57" :src="food.icon">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span class="count">月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span class="now">￥{{food.price}}</span>
+                    <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
+                  <div class="cartcontrol-wrapper">
+                    <cartcontrol @add="addFood" :food="food"></cartcontrol>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
-      <v-shopcart ref="shopcart" :selectFoods="selectFoods"
-      :deliveryPrice="seller.deliveryPrice"
-      :minPrice="seller.minPrice"></v-shopcart>
+      <shopcart 
+        ref="shopcart"
+        :selectFoods="selectFoods"
+        :deliveryPrice="seller.deliveryPrice"
+        :minPrice="seller.minPrice"
+      ></shopcart>
     </div>
   </div>
 </template>
 
 <script>
 import BScroll from 'better-scroll'
-import cartcontrol from '@/components/cartcontrol/cartcontrol.vue'
-import shopcart from '@/components/shopcart/shopcart.vue'
+import cartcontrol from '@/components/cartcontrol/cartcontrol'
+import shopcart from '@/components/shopcart/shopcart'
 export default {
   name: 'Goods',
-  props:{
+  props: {
     seller: {
-      type:Object
+      type: Object
     }
   },
   data () {
     return {
-      classMap: [],
       goods: [],
-      listHeight:[],
+      listHeight: [],
       scrollY: 0
     }
   },
-  computed:{
+  computed: {
     currentIndex () {
-      for (let i = 0; i < this.listHeight.lenght; i++) {
+      for (let i = 0; i < this.listHeight.length; i++) {
         let height1 = this.listHeight[i]
         let height2 = this.listHeight[i + 1]
         if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
@@ -89,32 +90,48 @@ export default {
       }
       return 0
     },
-    selectFoods() {
-     let foods = []
-     this.goods.forEach(good =>{
-       good.foods.forEach(food => {
-         if (food.count) {
-           foods.push(food)
-         }
-       })
-     })
-     return foods
+    selectFoods () {
+      let foods = []
+      this.goods.forEach(good => {
+        good.foods.forEach(food => {
+          if (food.count) {
+            foods.push(food)
+          }
+        })
+      })
+      return foods
     }
   },
   components: {
-    'v-cartcontrol': cartcontrol,
-    'v-shopcart': shopcart
+    cartcontrol,
+    shopcart
   },
   methods: {
-    selectMenu(index, event){
-      // if(!event._constructed){
-      //   return
-      // }
+    selectMenu (index, event) {
+      if (!event._constructed) {
+        return
+      }
       let foodList = this.$refs.foodList
       let el = foodList[index]
-      this.foodsScroll.scrollToElement(el,300)
+      this.foodsScroll.scrollToElement(el, 300)
     },
-    _initScroll () {
+    selectFood (food, event) {
+      if (!event._constructed) {
+        return
+      }
+      this.selectFood = food
+      this.$refs.food.show()
+    },
+    addFood(target) {
+      this._drop(target);
+    },
+    _drop(target) {
+      // 体验优化,异步执行下落动画
+      this.$nextTick(() => {
+        this.$refs.shopcart.drop(target);
+      });
+    },
+    _initScroll() {
       this.menuScroll = new BScroll(this.$refs.menuWrapper, {
         click: true
       })
@@ -123,41 +140,32 @@ export default {
         probeType: 3
       })
       this.foodsScroll.on('scroll', pos => {
+        // console.log(pos)
         this.scrollY = Math.abs(Math.round(pos.y))
-        console.log(this.scrollY)
       })
-    }
-  },
-  _drop(target){
-    // 体验优化 异步执行下落动画
-    this.$nextTick(()=>{
-      
-    })
-  },
-  addFood(target){
-    this._drop(target)
-  },
-  _caculateHeight() {
-    let foodList = this.$refs.foodList
-    let height = 0
-    this.listHeight.push(height)
-    for (let i = 0;i<foodList.length;i++) {
-      let item = foodList[i]
-      height += item.clientHeight
+    },
+    _calculateHeight () {
+      let foodList = this.$refs.foodList
+      let height = 0
       this.listHeight.push(height)
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
     }
   },
   created () {
     this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
 
-    this.$http.get('https://www.easy-mock.com/mock/5ca457f04767c3737055c868/example/vue-ele2')
+    this.$http.get('https://www.easy-mock.com/mock/5ca2c29464930718b239eb94/lm/vue-eleme-goods')
       .then(res => {
-        // console.log(res)
+        console.log(res)
         if (res.data.errno === 0) {
           this.goods = res.data.data
           this.$nextTick(() => { //页面渲染完成才能执行
             this._initScroll()
-            this._caculateHeight()
+            this._calculateHeight()
           })
         }
       })
@@ -278,5 +286,5 @@ export default {
         .cartcontrol-wrapper
           position absolute
           right 0
-          bottom 5px
+          bottom 12px
 </style>
---
+++ vue-learn/vuex/vuex-counter/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+  <button @click="decrement">-</button>
+   {{$store.state.count}}
+   <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+//借
+import {mapActions} from 'vuex';
+export default {
+  
+  name: 'App',
+  methods:mapActions([
+    'increment',
+    'decrement'
+  ]),
+  components:{
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
